c50181861dfb9df81b1c55fa6b7893214f54afe1
Hernan Rubio <hernan.andres.rubio@gmail.com>
2023-11-03 19:41:59 -0300

    dashboard page added

--- src/main/java/com/unnoba/ClubDeSocios/controlles/DashboardController.java
+++ src/main/java/com/unnoba/ClubDeSocios/controlles/DashboardController.java
@@ -4,11 +4,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class UserController {
+public class DashboardController {
+  
+  @GetMapping("/dashboard")
+  public String Dashboard(){
+    return "dashboard";
+  }
 
-    @GetMapping("/")
-    public String Login(){
-        return "login";
-    }
-    
 }
