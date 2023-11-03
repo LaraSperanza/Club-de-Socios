23f0f8f297386e6ba8efab178252c5df0321ea9b
Hernan Rubio <hernan.andres.rubio@gmail.com>
2023-11-02 22:31:13 -0300

    login form added

--- src/main/java/com/unnoba/ClubDeSocios/controlles/UserController.java
+++ src/main/java/com/unnoba/ClubDeSocios/controlles/UserController.java
@@ -5,10 +5,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/")
 public class UserController {
 
-    @GetMapping("/login")
+    @GetMapping("/")
     public String Login(){
         return "login";
     }
