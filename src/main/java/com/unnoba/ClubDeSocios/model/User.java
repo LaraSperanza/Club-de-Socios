b55c9df83a5dba2da9cc613dd1e0252a63ad644d
Hernan Rubio <hernan.andres.rubio@gmail.com>
2023-11-17 15:34:41 -0300

    security removed

--- src/main/java/com/unnoba/ClubDeSocios/model/User.java
+++ src/main/java/com/unnoba/ClubDeSocios/model/User.java
@@ -1,12 +1,5 @@
 package com.unnoba.ClubDeSocios.model;
 
-import java.util.Arrays;
-import java.util.Collection;
-
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -21,7 +14,7 @@ import lombok.Setter;
 @Getter //Getter y Setter nos ahorra escribir éstos.
 @Setter
 @AllArgsConstructor //Aplica un construtor con todos los atributos.
-public class User implements UserDetails{
+public class User{
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -30,33 +23,5 @@ public class User implements UserDetails{
     private String name;
     private String surname;
     private String password;
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        try {
-            return Arrays.asList(new SimpleGrantedAuthority("ROLE_USER"));
-        } catch (Exception e) {
-            throw new UnsupportedOperationException("Unimplemented method 'getAuthorities'");
-        }
-    }
-    @Override
-    public String getUsername() {
-        
-        throw new UnsupportedOperationException("Unimplemented method 'getUsername'");
-    }
-    @Override
-    public boolean isAccountNonExpired() {
-        return true;
-    }
-    @Override
-    public boolean isAccountNonLocked() {
-        return true;
-    }
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return true;
-    }
-    @Override
-    public boolean isEnabled() {
-        return true;
-    }
+   
 }
